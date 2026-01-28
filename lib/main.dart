1915b101eac196de8d80a05d8a5c681908c51cc1
alejandrolsr_ <alopsal2504@g.educaand.es>
2026-01-28 14:14:02 +0100

    Conexion con Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; 
 import 'screens/splash_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
 
-void main() {
+void main() async {
+ 
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -17,7 +26,7 @@ class MyApp extends StatelessWidget {
       title: 'Miramar Maintenance',
 
       // SISTEMA DE RUTAS
-      initialRoute: 'splash', //Iniciamos en el splash
+      initialRoute: 'splash', // Iniciamos en el splash
       routes: {
         'splash': (context) => const SplashScreen(),
         'login': (context) => const LoginScreen(),
